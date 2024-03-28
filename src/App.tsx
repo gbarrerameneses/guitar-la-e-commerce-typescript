7886c2ae30ad8ca05f4a24ba2720d0e48216dddd
Gustavo Barrera <gustavo.8805@gmail.com>
2024-03-28 09:35:54 -0600

    corrigiendo extensión app.tsx, corrección main.tsx y creando interface para la data

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Header from "./components/Header";
 import Guitar from "./components/Guitar";
 import  useCart  from "./hooks/useCart";
@@ -5,6 +6,8 @@ import  useCart  from "./hooks/useCart";
 function App() {
 
   const { data, cart, addToCart, removeToCart, decreaseQuantity, increaseQuantity, clearCart, isEmpty, cartTotal } = useCart()
+
+  const [auth, setAuth] = useState(false)
 
   return (
     <>
--- src/data/db.ts
+++ src/data/db.ts
@@ -1,4 +1,14 @@
-export const db = [
+//type Guitar = {
+interface Guitar {
+    id: number
+    name: string
+    image: string
+    description: string
+    price: number
+}
+
+
+export const db : Guitar[] = [
     {
         id: 1,
         name: 'Lukather',
